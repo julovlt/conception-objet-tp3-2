fc948d223413d03d47d15754e59136f16b71685e
Kevin Bougeois <kevin.bourgeois@univ-orleans.fr>
2021-02-09 15:53:04 +0100

    creation du test test_box_in

--- tests_tp2.py
+++ tests_tp2.py
@@ -7,3 +7,12 @@ def test_box_add():
     b = Box()
     b.add("truc1")
     b.add("truc2")
+
+
+def test_box_in():
+    b = Box()
+    b.add("truc1")
+    b.add("truc2")
+
+    assert "truc1" in b
+    assert "truc2" in b
