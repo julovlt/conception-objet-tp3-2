5d2b77872399ca96ca8976755e0980c4b7aedeba
Kevin Bougeois <kevin.bourgeois@univ-orleans.fr>
2021-02-16 16:09:11 +0100

    ajout du test de capacité

--- tests_tp2.py
+++ tests_tp2.py
@@ -47,3 +47,12 @@ def test_action_look():
 def test_thing_create():
     t = Thing(3)
     assert t.volume() == 3
+
+def test_box_capacity():
+    b = Box()
+
+    assert b.capacity() is None
+
+    b.set_capacity(5)
+
+    assert b.capacity() == 5
